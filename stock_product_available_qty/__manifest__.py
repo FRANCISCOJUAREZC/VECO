a7c0b30225a1b0ffb43f6d99b0e550fcacbbd718
FRANCISCOJUAREZC <paco_juarez@yahoo.com>
2020-03-03 12:53:04 -0600

    update calculo cantidad en mano

---
+++ stock_product_available_qty/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# © 2020 Morwi Encoders Consulting SA DE CV
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'Stock Product Available Qty',
+    'version': '12.0.1.0.0',
+    'author': 'Morwi Encoders Consulting SA DE CV',
+    'category': 'Inventory',
+    'website': 'http://www.morwi.mx/',
+    'license': 'LGPL-3',
+    'summary': 'Only get the physical locations to compute stock inventory',
+    'depends': ['mrp'],
+}
---
+++ stock_product_available_qty/models/product.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# © 2020 Morwi Encoders Consulting SA DE CV
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import models
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    def _get_domain_locations_new(self, location_ids,
+                                  company_id=False, compute_child=True):
+        domain_quant_loc, domain_move_in_loc, domain_move_out_loc = super(
+            ProductProduct, self)._get_domain_locations_new(location_ids,
+                                                            company_id,
+                                                            compute_child)
+        locations = self.env['stock.location'].browse(location_ids)
+        operator = compute_child and 'child_of' or 'in'
+        hierarchical_locations = (
+            locations if operator == 'child_of' else locations.browse())
+        wrong_locations = []
+        for location in hierarchical_locations:
+            warehouse = location.get_warehouse()
+            # Pre-Production
+            wrong_locations.append(warehouse.sam_loc_id.id)
+            # Post-Production
+            wrong_locations.append(warehouse.pbm_loc_id.id)
+        domain_quant_loc.append(
+            ('location_id', 'not in', wrong_locations))
+        domain_move_in_loc.append(
+            ('location_dest_id', 'not in', wrong_locations))
+        domain_move_out_loc.append(
+            ('location_id', 'not in', wrong_locations))
+        return (domain_quant_loc, domain_move_in_loc, domain_move_out_loc)
