51d2348765008308a903a6f0b46b5b0b26044f37
FRANCISCOJUAREZC <paco_juarez@yahoo.com>
2020-04-29 16:07:48 -0500

    gasto con so bloqueadas

---
+++ veco_customizations/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2020 Morwi Encoders Consulting SA DE CV
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'Veco Customizations',
+    'version': '12.0.1.0.0',
+    'author': 'Morwi Encoders Consulting SA DE CV',
+    'category': 'Hidden',
+    'website': 'http://www.morwi.mx/',
+    'license': 'LGPL-3',
+    'summary': 'Specific customizations for Veco',
+    'depends': [
+        'l10n_mx_edi',
+        'hr_expense'
+    ],
+}
--- veco_customizations/models/__init__.py
+++ veco_customizations/models/__init__.py
@@ -2,6 +2,5 @@
 # © 2020 Morwi Encoders Consulting SA DE CV
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from . import account_move
-from . import mrp_workorder
-from . import stock_warehouse
+from . import account_invoice
+from . import analytic
