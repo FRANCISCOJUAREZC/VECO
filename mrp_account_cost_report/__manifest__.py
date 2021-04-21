56b9cd3d0e8f519a3d3197f9dcd95b530f915436
ougc27 <oscar.garza@morwi.mx>
2021-04-20 21:04:28 -0500

    [ADD] mrp_account_cost_report: Modulo de gastos de manufactura.

---
+++ mrp_account_cost_report/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2021 Morwi Encoders Consulting SA DE CV
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'MRP Cost Report',
+    'version': '12.0.1.0.0',
+    'author': 'Morwi Encoders Consulting SA DE CV',
+    'category': 'Manufacture',
+    'website': 'http://www.morwi.mx/',
+    'license': 'LGPL-3',
+    'summary': 'MRP Cost Report Analysis',
+    'depends': [
+        'sale',
+        'mrp_workorder',
+        'mrp_account',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'report/manufacture_cost_report.xml',
+    ]
+}
