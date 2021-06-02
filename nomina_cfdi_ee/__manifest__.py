f30b81806b6514df71a18a41be2066a1eeee9f28
Fernando Rivero <frivero@itadmin.com.mx>
2021-06-02 18:19:54 -0500

    [FIX] Change version

--- nomina_cfdi_ee/__manifest__.py
+++ nomina_cfdi_ee/__manifest__.py
@@ -7,7 +7,7 @@
     Nomina CFDI Module
     ''',
     'author': 'IT Admin',
-    'version': '12.29',
+    'version': '12.30',
     'category': 'Employees',
     'depends': [
         'base', 'hr', 'hr_payroll', 'hr_payroll_account'
