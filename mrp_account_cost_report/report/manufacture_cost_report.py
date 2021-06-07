7ba15a96dbd2521da2dc376671e8779485b242dc
Fernando Rivero <frivero@itadmin.com.mx>
2021-06-07 13:15:19 -0500

    [FIX] Comment eror

--- mrp_account_cost_report/report/manufacture_cost_report.py
+++ mrp_account_cost_report/report/manufacture_cost_report.py
@@ -103,5 +103,5 @@ class ManufactureCostReport(models.Model):
     @api.model_cr
     def init(self):
         tools.drop_view_if_exists(self.env.cr, self._table)
-        self.env.cr.execute("""CREATE or REPLACE VIEW %s as (%s)""" % (
-            self._table, self._query()))
+ #       self.env.cr.execute("""CREATE or REPLACE VIEW %s as (%s)""" % (
+ #           self._table, self._query()))
