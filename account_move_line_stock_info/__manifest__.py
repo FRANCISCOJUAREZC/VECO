6d7de1a736fe5ea7650c291e71ca4afc527e2e1d
ougc27 <oscar.garza@morwi.mx>
2022-03-03 10:34:46 -0600

    [ADD] account_move_line_stock_info: Module migration to V15

--- account_move_line_stock_info/__manifest__.py
+++ account_move_line_stock_info/__manifest__.py
@@ -1,20 +1,17 @@
-# © 2016 Eficent Business and IT Consulting Services S.L.
+# © 2016 ForgeFlow S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Move Line Stock Info",
-    "version": "12.0.1.0.0",
-    "depends": [
-        "stock_account"
-    ],
-    "author": "Eficent,"
-              "Odoo Community Association (OCA)",
+    "version": "15.0.1.0.0",
+    "depends": ["stock_account"],
+    "author": "ForgeFlow," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse Management",
     "installable": True,
     "license": "AGPL-3",
     "data": [
-        'security/ir.model.access.csv',
-        'views/account_move_line_view.xml',
-        'views/stock_move_view.xml',
-    ]
+        "security/ir.model.access.csv",
+        "views/account_move_line_view.xml",
+        "views/stock_move_view.xml",
+    ],
 }
--- account_move_line_stock_info/readme/CONTRIBUTORS.rst
+++ account_move_line_stock_info/readme/CONTRIBUTORS.rst
@@ -1,2 +1,4 @@
-* Jordi Ballester Alomar <jordi.ballester@eficent.com>
-* Lois Rilo <lois.rilo@eficent.com>
+* Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
+* Lois Rilo <lois.rilo@forgeflow.com>
+* Pimolnat Suntian <pimolnats@ecosoft.co.th>
+* Joan Mateu <joan.mateu@forgeflow.com>
