919b165f23057988718628af7ce3be2d8e22fd85
Oscar <oscar.garza@morwi.mx>
2022-06-14 17:34:44 -0500

    [FIX] mrp: Validate serial numbers to avoid duplicates.

--- mrp_automatic_tracking/models/mrp_workorder.py
+++ mrp_automatic_tracking/models/mrp_workorder.py
@@ -2,7 +2,8 @@
 # © 2020 Morwi Encoders Consulting SA DE CV
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import api, models
+from odoo import _, api, models
+from odoo.exceptions import UserError
 from odoo.tools import float_compare
 
 
@@ -30,3 +31,25 @@ class MrpWorkOrder(models.Model):
         if self.production_id.set_manual_tracking:
             return super(MrpWorkOrder, self)._generate_lot_ids()
         return True
+
+    def record_production(self):
+        """Check if the product manufactured has a serial duplicated"""
+        self.ensure_one()
+        if not self.next_work_order_id:
+            if self.production_id.product_id.tracking == 'serial':
+                production_moves = self.env['stock.move.line'].search(
+                    [('product_id', '=', self.production_id.product_id.id),
+                     ('move_id.production_id', '!=', False),
+                     ('state', '!=', 'cancel'),
+                     ('lot_id', '=', self.final_lot_id.id)])
+                if production_moves:
+                    orders = production_moves.mapped(
+                        'move_id.production_id.name')
+                    raise UserError(
+                        _('You must only have a unique serial number for this'
+                            ' product. \n This product actually is on the'
+                            ' following orders: \n\n %s ') %
+                        (('\n').join(orders)))
+
+        res = super(MrpWorkOrder, self).record_production()
+        return res
