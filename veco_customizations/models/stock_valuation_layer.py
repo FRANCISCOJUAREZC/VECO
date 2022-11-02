04092a22485f752e94c22b37964d08ba6490ffb8
Oscar <oscar.garza@morwi.mx>
2022-11-02 10:25:32 -0600

    [ADD] mrp_account_cost_report: Cron to refresh production orders cost values to report

--- veco_customizations/models/stock_valuation_layer.py
+++ veco_customizations/models/stock_valuation_layer.py
@@ -2,10 +2,20 @@
 # © 2020 Morwi Encoders Consulting SA DE CV
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockValuationLayer(models.Model):
     _inherit = 'stock.valuation.layer'
 
     unit_cost = fields.Monetary(readonly=False)
+    remaining_qty = fields.Monetary(readonly=False)
+
+    @api.onchange('unit_cost')
+    def _onchange_unit_cost(self):
+        self.value = self.unit_cost * self.quantity
+        self.remaining_value = self.unit_cost * self.remaining_qty
+
+    @api.onchange('remaining_qty')
+    def _onchange_remaining_qty(self):
+        self.remaining_value = self.unit_cost * self.remaining_qty
