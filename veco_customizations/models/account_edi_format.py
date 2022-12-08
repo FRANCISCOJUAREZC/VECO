a22b729747109947b53d9d61fc0df3896abe81fc
Oscar <oscar.garza@morwi.mx>
2022-12-08 11:05:50 -0600

    [FIX] veco_customizations: payment installments of payment on CFDI

--- veco_customizations/models/account_edi_format.py
+++ veco_customizations/models/account_edi_format.py
@@ -13,12 +13,17 @@ class AccountEdiFormat(models.Model):
             AccountEdiFormat, self)._l10n_mx_edi_get_payment_cfdi_values(move)
         for invoice_vals in result.get('invoice_vals_list', []):
             payments = invoice_vals['invoice']._get_reconciled_payments()
+            payments = payments.filtered(
+                lambda p: p.journal_id.type in ['bank', 'cash'])
             payments_count = len(payments)
-            reconciled_lines = invoice_vals['invoice'].line_ids.filtered(lambda line: line.account_id.user_type_id.type in ('receivable', 'payable'))
+            reconciled_lines = invoice_vals['invoice'].line_ids.filtered(
+                lambda line: line.account_id.user_type_id.type in ('receivable', 'payable'))
             reconciled_amls = reconciled_lines.mapped('matched_debit_ids.debit_move_id') + \
                 reconciled_lines.mapped('matched_credit_ids.credit_move_id')
             for reconciled_aml in reconciled_amls:
                 if reconciled_aml.payment_id and reconciled_aml.payment_id in payments:
+                    continue
+                if reconciled_aml.payment_id and reconciled_aml.journal_id.type not in ['bank', 'cash']:
                     continue
                 payments_count += 1
             invoice_vals['number_of_payments'] = payments_count
