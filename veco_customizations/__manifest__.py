038b17bf2b7952f0736b75a4092ac11254d88358
Oscar <oscar.garza@morwi.mx>
2023-05-12 15:28:11 -0600

    polizas

--- veco_customizations/__manifest__.py
+++ veco_customizations/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Veco Customizations',
-    'version': '15.0.1.0.1',
+    'version': '15.0.1.0.2',
     'author': 'Morwi Encoders Consulting SA DE CV',
     'category': 'Hidden',
     'website': 'http://www.morwi.mx/',
