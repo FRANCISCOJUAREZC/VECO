9bf356486e2a39972384f12015ea27729b53f462
Jonathan Sanchez <sanceznarval00@outlook.com>
2023-05-23 16:09:16 -0600

    Cambio de valor False -> True en "new_pr_line"

--- purchase_request/wizard/purchase_request_line_make_purchase_order.py
+++ purchase_request/wizard/purchase_request_line_make_purchase_order.py
@@ -270,8 +270,8 @@ class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
             alloc_uom = line.product_uom_id
             wizard_uom = item.product_uom_id
             if available_po_lines and not item.keep_description:
-                new_pr_line = False
-                po_line = available_po_lines[1]
+                new_pr_line = True
+                po_line = available_po_lines[0]
                 po_line.purchase_request_lines = [(4, line.id)]
                 po_line.move_dest_ids |= line.move_dest_ids
                 po_line_product_uom_qty = po_line.product_uom._compute_quantity(
