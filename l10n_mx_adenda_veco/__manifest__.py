6555aa099e4e575e54fa69199b9fe5dfa6f142b0
Fernando Rivero <frivero@itadmin.com.mx>
2024-05-18 11:10:18 -0500

    [ADD] adenda

---
+++ l10n_mx_adenda_veco/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#                 @author IT Admin
+#
+##############################################################################
+
+{
+    'name': 'Adenda OC',
+    'version': '15.01',
+    'description': ''' Agrega nodo de Adenda con la OC
+    ''',
+    'category': 'Accounting',
+    'author': 'IT Admin',
+    'website': 'odoo.itadmin.com.mx',
+    'depends': [
+        'sale', 'l10n_mx_edi',
+    ],
+    'data': [
+        #'security/ir.model.access.csv',
+        'views/account_invoice_view.xml',
+	],
+    'application': False,
+    'installable': True,
+}
