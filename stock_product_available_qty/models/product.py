85dcaf9999608394025a99d8383a53fff0b1d764
lmgr0312 <lmiguel.gruiz@gmail.com>
2026-04-29 00:28:39 -0600

    [FIX]purchase.order.line product_uom_id, mrp check_availability

--- stock_product_available_qty/models/product.py
+++ stock_product_available_qty/models/product.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo import models
-from odoo.osv import expression
+from odoo.fields import Domain
 
 
 class ProductProduct(models.Model):
@@ -36,42 +36,35 @@ class ProductProduct(models.Model):
     #         ('location_id', 'not in', wrong_locations))
     #     return (domain_quant_loc, domain_move_in_loc, domain_move_out_loc)
 
+
     def _get_domain_locations_new(self, *args, **kwargs):
-        # Ejecutamos el super de forma flexible
-        res = super(ProductProduct, self)._get_domain_locations_new(*args, **kwargs)
+        res = super()._get_domain_locations_new(*args, **kwargs)
         domain_quant_loc, domain_move_in_loc, domain_move_out_loc = res
 
-        # Extraemos los valores de los argumentos sin importar la posición
-        # location_ids suele ser el primer argumento en args o estar en kwargs
         location_ids = args[0] if args else kwargs.get('location_ids', [])
         compute_child = kwargs.get('compute_child', True)
 
         locations = self.env['stock.location'].browse(location_ids)
-        operator = 'child_of' if compute_child else 'in'
-        
-        # Filtrado de locaciones (tu lógica personalizada)
-        hierarchical_locations = locations if operator == 'child_of' else locations.browse()
+
         wrong_locations = []
-        for location in hierarchical_locations:
+        for location in locations:
             if location.warehouse_id.sam_loc_id:
                 wrong_locations.append(location.warehouse_id.sam_loc_id.id)
             if location.warehouse_id.pbm_loc_id:
                 wrong_locations.append(location.warehouse_id.pbm_loc_id.id)
 
-        # Aplicamos los filtros a los dominios obtenidos del super
-        # In Odoo 17+, domains may be DomainCondition objects instead of lists
         if wrong_locations:
-            def to_list(dom):
-                if isinstance(dom, list):
-                    return dom
-                try:
-                    return list(dom)
-                except TypeError:
-                    return [dom]
+            # Asegurar que todo sea Domain
+            if isinstance(domain_quant_loc, list):
+                domain_quant_loc = Domain(domain_quant_loc)
+            if isinstance(domain_move_in_loc, list):
+                domain_move_in_loc = Domain(domain_move_in_loc)
+            if isinstance(domain_move_out_loc, list):
+                domain_move_out_loc = Domain(domain_move_out_loc)
 
-            domain_quant_loc = expression.AND([to_list(domain_quant_loc), [('location_id', 'not in', wrong_locations)]])
-            domain_move_in_loc = expression.AND([to_list(domain_move_in_loc), [('location_dest_id', 'not in', wrong_locations)]])
-            domain_move_out_loc = expression.AND([to_list(domain_move_out_loc), [('location_id', 'not in', wrong_locations)]])
+            domain_quant_loc &= Domain('location_id', 'not in', wrong_locations)
+            domain_move_in_loc &= Domain('location_dest_id', 'not in', wrong_locations)
+            domain_move_out_loc &= Domain('location_id', 'not in', wrong_locations)
 
         return (domain_quant_loc, domain_move_in_loc, domain_move_out_loc)
 
