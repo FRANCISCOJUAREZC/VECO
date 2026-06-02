31ebd764dc2e5140babba2615d6c285468823358
lmgr0312 <lmiguel.gruiz@gmail.com>
2026-06-02 17:17:30 -0600

    [FIX]avoid run computes with context to avoid task kill

--- mrp_account_cost_report/models/__init__.py
+++ mrp_account_cost_report/models/__init__.py
@@ -2,4 +2,4 @@
 # © 2021 Morwi Encoders Consulting SA DE CV
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from . import mrp_production
+from . import mrp_production, sale_order
