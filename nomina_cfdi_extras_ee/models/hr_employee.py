041fdf46ff985754ea4614448293bfb717105b23
ougc27 <oscar.garza@morwi.mx>
2026-09-17 09:06:40 -0600

    [FIX] dependencias nominas

--- nomina_cfdi_extras_ee/models/hr_employee.py
+++ nomina_cfdi_extras_ee/models/hr_employee.py
@@ -7,9 +7,10 @@ class hr_employee(models.Model):
     _inherit = 'hr.employee'
 
     loan_request = fields.Integer('Solicitud del prestamo por año', default=1, required=True)
-
+    no_employee = fields.Char('No de empleado')
 
 class HrEmployeePublic(models.Model):
     _inherit = 'hr.employee.public'
 
     loan_request = fields.Integer('Solicitud del prestamo por año', default=1, required=True)
+    no_employee = fields.Char('No de empleado')
